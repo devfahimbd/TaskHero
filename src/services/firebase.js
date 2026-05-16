e24fd4e3d813b04c7c004fe9c0c4e0a0036e22f6
devfahimbd <learncodewithfahim@gmail.com>
2026-05-16 12:27:18 +0600

    Added Firebase Config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -20,11 +20,11 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 // TODO: Replace with your Firebase project config
 const firebaseConfig = {
   apiKey: 'AIzaSyBY5ul4EGOa60Qrhr2xL5ReNRtnh5Xa7HM',
-  authDomain: 'YOUR_PROJECT.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  authDomain: 'task-hero-f2d01.firebaseapp.com',
+  projectId: 'task-hero-f2d01',
+  storageBucket: 'task-hero-f2d01.firebasestorage.app',
+  messagingSenderId: '360271990389',
+  appId: '1:360271990389:web:1fa951c5d04d1c793bf23c',
 };
 
 // Initialize Firebase
